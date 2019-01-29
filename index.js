d06b0c19fe04c53d4f4e78a67045ba85247711b3
วีรภัทร แก่นจันทร์ <weerapat_p_1995@hotmail.com>
2019-01-29 21:09:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.TOKEN;
 
-client.on('ready', () => {
-  console.log('I am ready!');
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "ping"
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    message.channel.send('pong');
+  }
 });
-
-client.on('guildMemberAdd', member => {
-    // Send the message to a designated channel on a server:
-    const channel = member.guild.channels.find(ch => ch.name === 'member-log');
-    // Do nothing if the channel wasn't found on this server
-    if (!channel) return;
-    // Send the message, mentioning the member
-    channel.send(`Welcome to the server, ${member}`);
-  });
 client.login(token).catch(err => console.log(err));
