5ab969e8b2f0b0603acf49c5d6471421c70007bc
วีรภัทร แก่นจันทร์ <weerapat_p_1995@hotmail.com>
2019-01-29 14:57:30 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.env.TOKEN
+client.on('guildMemberAdd', member => {
+    // Send the message to a designated channel on a server:
+    const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+    // Do nothing if the channel wasn't found on this server
+    if (!channel) return;
+    // Send the message, mentioning the member
+    channel.send(`Welcome to the server, ${member}`);
+  });
+client.login(token).catch(err => console.log(err));
