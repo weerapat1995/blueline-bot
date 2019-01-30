d6c21a8eb825c39f20d45b0f9080690fa1552f47
วีรภัทร แก่นจันทร์ <weerapat_p_1995@hotmail.com>
2019-01-30 14:44:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ const client = new Discord.Client();
 const token = process.env.TOKEN;
 
 // Create an event listener for messages
-client.on('message', message => {
-  // If the message is "ping"
-  if (message.content === 'ping') {
-    // Send "pong" to the same channel
-    message.channel.send('pong');
-  }
-});
+client.on("ready", async ()=>{
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setActivity("กำลังคิดคำสั่ง", {type : "ใช้คำสั่ง"})
+})
 client.login(token).catch(err => console.log(err));
